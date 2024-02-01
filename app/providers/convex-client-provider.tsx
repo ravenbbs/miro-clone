76a865afd0e8d6b7c56b9caeca03d06f6ee8d8f7
Julio Condor <julioc3bgud@gmail.com>
2024-01-31 21:18:33 -0500

    Add convex -Auth Clerk

---
+++ app/providers/convex-client-provider.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+import { AuthLoading, Authenticated, ConvexReactClient } from "convex/react";
+
+interface ConvexClientProviderProps {
+  children: React.ReactNode;
+}
+
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
