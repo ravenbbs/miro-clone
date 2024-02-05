00cd2957ca8fdfdca7bfe11df4acaf782a3c4167
Julio Condor <julioc3bgud@gmail.com>
2024-02-04 21:08:37 -0500

    Add Convex client provider

--- providers/convex-client-provider.tsx
+++ providers/convex-client-provider.tsx
@@ -10,3 +10,18 @@ interface ConvexClientProviderProps {
 }
 
 const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
+
+const convex = new ConvexReactClient(convexUrl)
+
+export const ConvexClientProvider = ({
+  children
+}: ConvexClientProviderProps) => {
+  return(
+    <ClerkProvider>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex} >
+        {children}
+      </ConvexProviderWithClerk>
+
+    </ClerkProvider>
+  )
+}
